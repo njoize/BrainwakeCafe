051724374737b3ec6df6e1568ce298f04be8a7df
njoize <njoize@gmail.com>
2018-11-25 14:16:43 +0700

    Check Space OK

--- app/src/main/java/njoize/dkh/th/co/brainwakecafe/MainFragment.java
+++ app/src/main/java/njoize/dkh/th/co/brainwakecafe/MainFragment.java
@@ -2,10 +2,13 @@ package njoize.dkh.th.co.brainwakecafe;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -18,6 +21,43 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Login Controller
+        loginController();
+
+
+    } // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+                MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    myAlertDialog.normalDialog("Have Space", "Please Fill Every Blank");
+
+                } else {
+//                    No Space
+
+                }
+
+
+            }
+        });
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
